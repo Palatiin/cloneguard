93e3496c94033b2275fa7eb9bb208cb7f3fad672
Matus Remen <xremen01@stud.fit.vutbr.cz>
2023-02-11 16:32:39 +0100

    BS: context extractor docs

--- coinwatch/src/context_extractor.py
+++ coinwatch/src/context_extractor.py
@@ -7,20 +7,40 @@ from typing import List, Optional, Tuple
 
 @dataclass
 class Context:
+    """Data class representing context and its keywords."""
+
     is_eof: bool = False
     keywords: List[str] = field(default_factory=list)
 
 
 class Extractor:
+    """Extractor of patch context."""
+
     _re_token = re.compile(r"[a-zA-Z0-9!._]+")
 
     def __init__(self, context_lines: Optional[int] = 5):
         self.context_lines = context_lines
 
     def _tokenize(self, line: str) -> List[str]:
+        """Tokenize line of code.
+
+        Args:
+            line (str): line of code
+
+        Returns:
+            Tokens from the line of code.
+        """
         return self._re_token.findall(line)
 
     def extract(self, patch: str) -> Tuple[Context, Context]:
+        """Extract Upper and Lower patch context.
+
+        Args:
+            patch (str): source code from patch
+
+        Returns:
+            Tuple with Upper and Lower context - keywords representing leading and trailing code context.
+        """
         patch_lines: List[str] = patch.split("\n")
         patch_lines = list(filter(lambda x: x, patch_lines))
 
