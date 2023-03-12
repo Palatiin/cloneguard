7f4ff16bf6ed3c69f61966e66320bd78e879cf1b
Matus Remen <xremen01@stud.fit.vutbr.cz>
2023-03-12 16:11:54 +0100

    BS: comparator: change similarity calculation

--- coinwatch/src/comparator.py
+++ coinwatch/src/comparator.py
@@ -3,7 +3,7 @@
 from enum import Enum
 from typing import List
 
-from nltk import edit_distance
+from similarity.normalized_levenshtein import NormalizedLevenshtein
 
 from coinwatch.settings import REWARD
 
@@ -16,21 +16,23 @@ class TypeOfPatch(int, Enum):
 
 class Comparator:
     _reward: float = REWARD
+    levenshtein = NormalizedLevenshtein()
 
-    @staticmethod
-    def _get_similarity(string_a: str, string_b: str) -> float:
+    @classmethod
+    def similarity(cls, string_a: str, string_b: str) -> float:
         """Normalize Levenshtein's distance metric."""
-        return edit_distance(string_a, string_b) / len(max([string_a, string_b], key=len))
+        return cls.levenshtein.similarity(string_a, string_b)
 
-    def compare(self, source: List[str], target: List[str]) -> float:
+    @classmethod
+    def compare(cls, source: List[str], target: List[str]) -> float:
         p: float = float(len(source))
 
         similarity_sum: float = 0.0
         for i, source_code in enumerate(source):
             similarities: List[float] = []
             for target_code in target:
-                similarities.append(self._get_similarity(source_code, target_code))
-            most_similar_index, value = min(enumerate(similarities), key=lambda x: x[1])
-            similarity_sum += value * self._reward ** (abs(i - most_similar_index))
+                similarities.append(cls.similarity(source_code, target_code))
+            most_similar_index, value = max(enumerate(similarities), key=lambda x: x[1])
+            similarity_sum += value * cls._reward ** (abs(i - most_similar_index))
 
         return similarity_sum / p
