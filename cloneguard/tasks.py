cbf9c8cb25389d8de430c9289221244550a12154
Matus Remen <xremen01@stud.fit.vutbr.cz>
2023-05-05 15:21:31 +0200

    chore(tasks): optimalize detection task

--- cloneguard/tasks.py
+++ cloneguard/tasks.py
@@ -4,16 +4,17 @@
 # Date: 2023-04-28
 # Description: Worker tasks
 
+from copy import deepcopy
 import os
 
 import structlog
-
-from copy import deepcopy
 
 import cloneguard.src.db.crud as crud
 from cloneguard.clients.detection_methods import Simian, BlockScope
 from cloneguard.clients.git import Git
 from cloneguard.src.db.session import db_session
+from cloneguard.src.fixing_commits import FixCommitFinder
+from cloneguard.src.notifications import Postman
 from cloneguard.src.update_repos import get_repo_objects, update_repos
 import cloneguard.settings as settings
 
@@ -31,7 +32,7 @@ def execute_task(bug_id: str, commit: str, patch: str, method: str, project_name
     if not os.path.exists(f"{settings.CACHE_PATH}/logs"):
         os.makedirs(f"{settings.CACHE_PATH}/logs")
     settings.configure_logging(f"{settings.CACHE_PATH}/logs/{timestamp}.log")
-    logger.info("Starting detection method...")
+    logger.info("Starting detection method...", bug_id=bug_id, commit=commit)
     bug = crud.bug.get_cve(db_session, bug_id)
     bug = deepcopy(bug)
     bug.commits = [commit]
@@ -46,8 +47,9 @@ def execute_task(bug_id: str, commit: str, patch: str, method: str, project_name
     clones = get_repo_objects(source=repo)
     update_repos(clones, date)
 
+    method_class = Simian if method == "simian" else BlockScope
+    detection_method = method_class(repo, bug)
     for clone in clones:
-        detection_method = (Simian if method == "simian" else BlockScope)(repo, bug)
         detection_method.run(clone)
 
     logger.info("Detection method finished.")
