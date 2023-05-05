c0f48c3d2a3520bd98fa35c3bd8d4daa02994d21
Matus Remen <xremen01@stud.fit.vutbr.cz>
2023-05-05 18:51:11 +0200

    chore(git): error handling

--- cloneguard/clients/git.py
+++ cloneguard/clients/git.py
@@ -37,6 +37,9 @@ class Git:
     def __init__(self, project: str | Project):
         project = project if isinstance(project, Project) else crud.project.get_by_name(db_session, project)
 
+        if not project:
+            raise Exception(f"Project {project} not found in database.")
+
         self.id = project.id
         self.url = project.url
         self.owner = project.author
